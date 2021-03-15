2fa656b491f44a0d1d354460bb74eb4f1561c09a
Chung Le <ledinhchung.it@gmail.com>
2021-03-15 22:45:58 +0700

    Practice AWS CDK

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,8 @@
+import * as cdk from '@aws-cdk/core';
+
+export class ServerlessPipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+  }
+}
